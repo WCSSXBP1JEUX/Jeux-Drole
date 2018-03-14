515697549f55fb2d93d8e226bd1b6223841e43b8
Zowac <kevinvanderstracten@gmail.com>
2018-03-14 17:37:33 +0100

    ajustement de l'affichage sur la page event à venir

--- page3.php
+++ page3.php
@@ -24,7 +24,7 @@
 	</div>
 
 	<div class="container-fluid bg">
-		<div class="container beige">
+		<div class="container beige col-lg-offset-2 col-lg-8">
 
 			<h1 class="col-xs-offset-3 col-sm-offset-4 col-lg-offset-5 col-6 espacement">Contact</h1>
 
--- page4.php
+++ page4.php
@@ -27,22 +27,22 @@
 		<div class="container beige col-lg-offset-2 col-lg-8">
 
 
-			<h1 class="col-sm-offset-4 col-lg-offset-4 espacement">Event à venir</h1>
+			<h1 class="col-xs-offset-2 col-sm-offset-4 col-lg-offset-4 espacement">Event à venir</h1>
 
 
 
 			<figure class="row">
 
-			<img class="col-md-4" src="Images/Colon_catane.jpg">
+			<img class="col-xs-offset-2 col-sm-offset-2 col-md-4" src="Images/Colon_catane.jpg">
 
 				<figcaption class="col-md-7">
-				<p>Nom de l’event : Soirée Catane</p>
-				<p>Date : samedi 15 Mars 19H30</p>
-				<p>Lieu : 34 rue des colons - 67404 KAHFSHEIM</p>
-				<p>Nom de l’organisateur : Pedro Vasquez</p>
-				<p>Participant max : 6</p>
-				<p>Description de l’event : Salut! j’organise une petite soirée pour jouer à Colon de Catane avec l’extension marins et les extensions 5 / 6 joueurs du jeu de base et de marins....</p>
-				<a href="page2.php" class="btn btn-light form-control change">Info</a>
+					<p>Nom de l’event : Soirée Catane</p>
+					<p>Date : samedi 15 Mars 19H30</p>
+					<p>Lieu : 34 rue des colons - 67404 KAHFSHEIM</p>
+					<p>Nom de l’organisateur : Pedro Vasquez</p>
+					<p>Participant max : 6</p>
+					<p>Description de l’event : Salut! j’organise une petite soirée pour jouer à Colon de Catane avec l’extension marins et les extensions 5 / 6 joueurs du jeu de base et de marins....</p>
+					<a href="page2.php" class="btn btn-light form-control change">Info</a>
 				</figcaption>
 
 			</figure>
@@ -50,17 +50,17 @@
 
 			<figure class="row">
 
-			<img class="col-md-4 col-md-push-7" src="Images/role-playing-game-2536016__340.jpg">
+			<img class="col-xs-offset-2 col-sm-offset-2 col-md-4 col-md-push-7" src="Images/role-playing-game-2536016__340.jpg">
 
-			<figcaption class="col-md-7 col-md-pull-4">
-			<p>Nom de l’event : Découverte JDR plenilunio</p>
-			<p>Date : dimanche 16 Mars 14h00</p>
-			<p>Lieu : 69 rue des archanges - 67404 KAHFSHEIM</p>
-			<p>Nom de l’organisateur : Emilie Martin</p>
-			<p>Participant max : 5</p>
-			<p>Description de l’event : La fin du monde nous a frappé de plein fouet, les solaires et les lunaires se livrent une bataille sans merci à la surface de notre planète...</p>
-			<a href="#" class="btn btn-light form-control change">Info</a>
-			</figcaption>
+				<figcaption class="col-md-7 col-md-pull-4">
+					<p>Nom de l’event : Découverte JDR plenilunio</p>
+					<p>Date : dimanche 16 Mars 14h00</p>
+					<p>Lieu : 69 rue des archanges - 67404 KAHFSHEIM</p>
+					<p>Nom de l’organisateur : Emilie Martin</p>
+					<p>Participant max : 5</p>
+					<p>Description de l’event : La fin du monde nous a frappé de plein fouet, les solaires et les lunaires se livrent une bataille sans merci à la surface de notre planète...</p>
+					<a href="#" class="btn btn-light form-control change">Info</a>
+				</figcaption>
 
 			</figure>
 
@@ -68,17 +68,17 @@
 
 			<figure class="row">
 
-			<img class="col-sm-6 col-md-4" src="Images/sherlock-holmes-resize.png">
+			<img class="col-xs-offset-2 col-sm-offset-3 col-sm-6 col-md-4" src="Images/sherlock-holmes-resize.png">
 
-			<figcaption class="col-md-7">
-			<p>Nom de l’event : Soirée Murder</p>
-			<p>Date : samedi 30 Mars 19h00</p>
-			<p>Lieu : 3 Impasse des bouchers - 67404 KAHFSHEIM</p>
-			<p>Nom de l’organisateur : Agatha Poirot</p>
-			<p>Participant max : 12</p>
-			<p>Description de l’event : 1922, la célèbre actrice Maya Toitovna organise une soirée mysticisme dans sa résidence de campagne. Malgré des invitations soigneusement fournies en main propre elle a reçu des menaces de mort...</p>
-			<a href="#" class="btn btn-light form-control change">Info</a>
-			</figcaption>
+				<figcaption class="col-xs-12 col-sm-12 col-md-7">
+					<p>Nom de l’event : Soirée Murder</p>
+					<p>Date : samedi 30 Mars 19h00</p>
+					<p>Lieu : 3 Impasse des bouchers - 67404 KAHFSHEIM</p>
+					<p>Nom de l’organisateur : Agatha Poirot</p>
+					<p>Participant max : 12</p>
+					<p>Description de l’event : 1922, la célèbre actrice Maya Toitovna organise une soirée mysticisme dans sa résidence de campagne. Malgré des invitations soigneusement fournies en main propre elle a reçu des menaces de mort...</p>
+					<a href="#" class="btn btn-light form-control change">Info</a>
+				</figcaption>
 
 			</figure>
 
