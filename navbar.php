ea1284f05030137ead85571dda1933c42194954d
cpailler <aprel.illectemn@gmail.com>
2018-03-14 17:04:27 +0100

    correction navbar

--- navbar.php
+++ navbar.php
@@ -1,14 +1,13 @@
 
 
 <div class="navbar navbar-default">
-
 	<a class="navbar-brand"><span>Jeux Drôles</span></a>
 
 	<div class="hidden-xs">
 	<ul class="nav navbar-nav">
-	    <li<?php if($_GET['page']=="page1.php")   { echo " class=\"active\""; } ?>><a href="page1.php?page=page1.php">Home</a></li>
-	    <li<?php if($_GET['page']=="page4.php")   { echo " class=\"active\""; } ?>><a href="page4.php?page=page4.php">Evénements à venir</a></li>
-	    <li<?php if($_GET['page']=="page3.php")	  { echo " class=\"active\""; } ?>><a href="page3.php?page=page3.php">Contact</a></li>
+	    <li<?php if($_SERVER['PHP_SELF']=="/page1.php")   { echo " class=\"active\""; } ?>><a href="page1.php">Home</a></li>
+	    <li<?php if($_SERVER['PHP_SELF']=="/page4.php")   { echo " class=\"active\""; } ?>><a href="page4.php">Evénements à venir</a></li>
+	    <li<?php if($_SERVER['PHP_SELF']=="/page3.php")	  { echo " class=\"active\""; } ?>><a href="page3.php">Contact</a></li>
 	</ul>
 	</div>
 
