a4dfd8764eb8d72ccbd647a7a28604cf1f039bb6
Zowac <kevinvanderstracten@gmail.com>
2018-03-15 09:52:31 +0100

    Travail sur le côté responsive de la page4.php

--- page4.php
+++ page4.php
@@ -33,34 +33,34 @@
 
 			<figure class="row">
 
-			<img class="col-xs-offset-2 col-sm-offset-2 col-md-4" src="Images/Colon_catane.jpg">
+			<img class="col-xs-offset-2 col-sm-offset-2 col-md-offset-0 col-md-4" src="Images/Colon_catane.jpg">
 
-				<figcaption class="col-md-7">
-					<p>Nom de l’event : Soirée Catane</p>
-					<p>Date : samedi 15 Mars 19H30</p>
-					<p>Lieu : 34 rue des colons - 67404 KAHFSHEIM</p>
-					<p>Nom de l’organisateur : Pedro Vasquez</p>
-					<p>Participant max : 6</p>
-					<p>Description de l’event : Salut! j’organise une petite soirée pour jouer à Colon de Catane avec l’extension marins et les extensions 5 / 6 joueurs du jeu de base et de marins....</p>
-					<a href="page2.php" class="btn btn-light form-control change">Info</a>
-				</figcaption>
+			<figcaption class="col-md-7">
+			<p>Nom de l’event : Soirée Catane</p>
+			<p>Date : samedi 15 Mars 19H30</p>
+			<p>Lieu : 34 rue des colons - 67404 KAHFSHEIM</p>
+			<p>Nom de l’organisateur : Pedro Vasquez</p>
+			<p>Participant max : 6</p>
+			<p>Description de l’event : Salut! j’organise une petite soirée pour jouer à Colon de Catane avec l’extension marins et les extensions 5 / 6 joueurs du jeu de base et de marins....</p>
+			<a href="page2.php" class="btn btn-light form-control change">Info</a>
+			</figcaption>
 
 			</figure>
 
 
 			<figure class="row">
 
-			<img class="col-xs-offset-2 col-sm-offset-2 col-md-4 col-md-push-7" src="Images/role-playing-game-2536016__340.jpg">
+			<img class="col-xs-offset-2 col-sm-offset-2 col-md-offset-0 col-md-4 col-md-push-7" src="Images/role-playing-game-2536016__340.jpg">
 
-				<figcaption class="col-md-7 col-md-pull-4">
-					<p>Nom de l’event : Découverte JDR plenilunio</p>
-					<p>Date : dimanche 16 Mars 14h00</p>
-					<p>Lieu : 69 rue des archanges - 67404 KAHFSHEIM</p>
-					<p>Nom de l’organisateur : Emilie Martin</p>
-					<p>Participant max : 5</p>
-					<p>Description de l’event : La fin du monde nous a frappé de plein fouet, les solaires et les lunaires se livrent une bataille sans merci à la surface de notre planète...</p>
-					<a href="#" class="btn btn-light form-control change">Info</a>
-				</figcaption>
+			<figcaption class="col-md-7 col-md-pull-4">
+			<p>Nom de l’event : Découverte JDR plenilunio</p>
+			<p>Date : dimanche 16 Mars 14h00</p>
+			<p>Lieu : 69 rue des archanges - 67404 KAHFSHEIM</p>
+			<p>Nom de l’organisateur : Emilie Martin</p>
+			<p>Participant max : 5</p>
+			<p>Description de l’event : La fin du monde nous a frappé de plein fouet, les solaires et les lunaires se livrent une bataille sans merci à la surface de notre planète...</p>
+			<a href="#" class="btn btn-light form-control change">Info</a>
+			</figcaption>
 
 			</figure>
 
@@ -68,22 +68,24 @@
 
 			<figure class="row">
 
-			<img class="col-xs-offset-2 col-sm-offset-3 col-sm-6 col-md-4" src="Images/sherlock-holmes-resize.png">
+			<img class="col-xs-offset-2 col-sm-offset-3 col-md-offset-0 col-sm-6 col-md-4" src="Images/sherlock-holmes-resize.png">
 
-				<figcaption class="col-xs-12 col-sm-12 col-md-7">
-					<p>Nom de l’event : Soirée Murder</p>
-					<p>Date : samedi 30 Mars 19h00</p>
-					<p>Lieu : 3 Impasse des bouchers - 67404 KAHFSHEIM</p>
-					<p>Nom de l’organisateur : Agatha Poirot</p>
-					<p>Participant max : 12</p>
-					<p>Description de l’event : 1922, la célèbre actrice Maya Toitovna organise une soirée mysticisme dans sa résidence de campagne. Malgré des invitations soigneusement fournies en main propre elle a reçu des menaces de mort...</p>
-					<a href="#" class="btn btn-light form-control change">Info</a>
-				</figcaption>
+			<figcaption class="col-xs-12 col-sm-12 col-md-7">
+			<p>Nom de l’event : Soirée Murder</p>
+			<p>Date : samedi 30 Mars 19h00</p>
+			<p>Lieu : 3 Impasse des bouchers - 67404 KAHFSHEIM</p>
+			<p>Nom de l’organisateur : Agatha Poirot</p>
+			<p>Participant max : 12</p>
+			<p>Description de l’event : 1922, la célèbre actrice Maya Toitovna organise une soirée mysticisme dans sa résidence de campagne. Malgré des invitations soigneusement fournies en main propre elle a reçu des menaces de mort...</p>
+			<a href="#" class="btn btn-light form-control change">Info</a>
+			</figcaption>
 
 			</figure>
 
 		</div>
 	</div>
+
+	
 
 	<div class="row container-fluid">
 
